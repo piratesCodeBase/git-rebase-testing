4ca0e6b9f27c8bccc6f49fff29ff25a2370250a5
DESKTOP-2APJII3\Admin <yogesh.kumar@collationhub.com>
2023-03-31 12:22:28 +0530

    This commit is the second commit done from feature branch

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ console.log("This is the second commit");
 console.log("This is the third commit on the main branch")
 
 console.log('This is the commit done from the feature branch');
+
+console.log("This is the second commit done from feature branch");
