2c3ef135a795a49f2f461dfeb1c52be07db6efda
DESKTOP-2APJII3\Admin <yogesh.kumar@collationhub.com>
2023-03-31 13:28:07 +0530

    This is fourth commit on master branch

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ console.log("This is the second commit");
 console.log("This is the third commit on the main branch")
 
 console.log('This is the commit done from the feature branch');
+
+console.log("This fourth commit is done from master branch");
