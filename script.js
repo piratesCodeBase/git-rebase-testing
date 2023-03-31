2e8297c02ee04882f498c604d17b20d4c414f1af
DESKTOP-2APJII3\Admin <yogesh.kumar@collationhub.com>
2023-03-31 14:32:29 +0530

    Rebase Commit

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ console.log("This is the third commit on the main branch")
 console.log('This is the commit done from the feature branch');
 
 console.log("This fourth commit is done from master branch");
+
+console.log("This is commit for the rebase");
