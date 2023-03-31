da2b50d1746be70b535610f9c14626234fd130c3
DESKTOP-2APJII3\Admin <yogesh.kumar@collationhub.com>
2023-03-31 12:41:41 +0530

    feature branch is squashed and merged into the master branch

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ console.log("Hello tthis is first message!!!");
 console.log("This is the second commit");
 
 console.log("This is the third commit on the main branch")
+
+console.log('This is the commit done from the feature branch');
+
+console.log("This is the second commit done from feature branch");
